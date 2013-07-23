1330c48695607ba6d8a38ad2170e5b1295799325
Reedy <reedy@wikimedia.org>
2013-07-23 15:20:56 +0100

    Fix double description
    Remove ?>
    
    Change-Id: If8d1092a138f6226e5443d2e7da6825baa4e3b42

--- sarcasm.php
+++ sarcasm.php
@@ -11,7 +11,7 @@
  * @link https://www.mediawiki.org/wiki/Extension:Sarcasm
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
- 
+
 /**
  * Protect against register_globals vulnerabilities.
  * This line must be present before any global variable is referenced.
@@ -20,36 +20,43 @@ if( !defined( 'MEDIAWIKI' ) ) {
         echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
         die( -1 );
 }
- 
-// Extension credits that will show up on Special:Version    
+
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['other'][] = array(
         'path'           => __FILE__,
         'name'           => 'Sarcasm',
         'version'        => '1.0',
-        'author'         => '[http://sturmkrieg.de/User:Inquisitor_Ehrenstein Inquisitor Ehrenstein]', 
+        'author'         => '[http://sturmkrieg.de/User:Inquisitor_Ehrenstein Inquisitor Ehrenstein]',
         'url'            => 'https://www.mediawiki.org/wiki/Extension:Sarcasm',
-        'descriptionmsg' => 'This extension adds sarcasm tags to the wiki in order to more clearly show sarcasm in text.',
-        'description'    => 'This extension adds sarcasm tags to the wiki.'
+        'description'    => 'This extension adds sarcasm tags to the wiki in order to more clearly show sarcasm in text.',
 );
 
-
 $wgHooks['ParserFirstCallInit'][] = 'wfSarcasmParserInit';
- 
-// Hook our callback function into the parser
+
+/**
+ * Hook our callback function into the parser
+ * @param $parser Parser
+ * @return bool
+ */
 function wfSarcasmParserInit( Parser $parser ) {
-        // When the parser sees the <sample> tag, it executes 
-        // the wfSampleRender function (see below)
+        // When the parser sees the <sarcasm> tag, it executes
+        // the wfSarcasmRender function (see below)
         $parser->setHook( 'sarcasm', 'wfSarcasmRender' );
         // Always return true from this function. The return value does not denote
         // success or otherwise have meaning - it just must always be true.
         return true;
 }
 
-// Code for tags
-
+/**
+ * Code for tags
+ *
+ * @param $text string
+ * @param $args array
+ * @param $parser Parser
+ * @param $frame PPFrame
+ * @return string
+ */
 function wfSarcasmRender( $text, array $args, Parser $parser, PPFrame $frame ) {
         $output = $parser->recursiveTagParse( $text, $frame );
         return '<span class="sarcasm">' . $output . '</span>';
 }
-
-?>
